e65ae9263965d09c55efffe8dcca85e381cea14d
danku001 <65789554+danku001@users.noreply.github.com>
2022-09-23 18:26:13 -0400

    Update data_splitter.py
    Changes made to explicitly set file permissions so program behaves as expected regardless of OS platform.

--- data_splitter.py
+++ data_splitter.py
@@ -2,24 +2,31 @@
 The purpose of this program is to read information
 from a large (40GB) file and separate it based on Tags or
 keywords
+
+Save file in the same location as the documents  to read
+Output files in the same location as script is in
 """
 
 
 import os
 import csv
-
+import oschmod
 
 #make sure you are in the same working directory as your file is in
-os.chdir("file location here")
 
+loc = os.getcwd()
 
-with open("tempValues.csv", mode = 'r', newline = '') as f:
+with open("tempValues.csv",
+          mode = 'r', newline = '') as f:
     #use context manager to open large file
     reader = csv.reader(f)
     for row in reader :
-        with open('Tag_' + row[0] + '.csv', mode = 'a',
+        file = os.path.join(loc, 'Tag_' + row[0].replace(' ','') + '.txt')
+        with open(file, mode = 'a+',
                   encoding = 'utf-8') as g:
-            writer = csv.writer(g)            
-            print(row)
+            assert os.path.isfile(file)
+            #explicitly setting permissions of file
+            oschmod.set_mode( os.path.basename(file), 'a+rwx')
+            writer = csv.writer(g)
             g.writelines( i + ' ' for i in row )
             g.write('\n')
